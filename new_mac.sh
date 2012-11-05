35eb35b5ee3d4d69a14940878b26a9dbb885ce86
Christopher Brown <io@henrian.com>
2012-11-04 21:11:23 -0600

    Add new mac (homebrew packages).

---
+++ new_mac.sh
@@ -0,0 +1,1 @@
+brew install ack autoconf automake bash curl dnsmasq gfortran jpegoptim libxml2 libyaml mongodb nginx node parallel phantomjs postgresql proctools redis ruby sqlite wget wkhtmltopdf youtube-dl
