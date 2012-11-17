41c2e6e6304530ef55516033d815ee956333fb0a
Christopher Brown <io@henrian.com>
2012-11-17 06:42:51 -0600

    Add finder quit option

--- new_mac.sh
+++ new_mac.sh
@@ -1,1 +1,2 @@
 brew install ack autoconf automake bash curl dnsmasq gfortran jpegoptim libxml2 libyaml mongodb nginx node parallel phantomjs postgresql proctools redis ruby sqlite wget wkhtmltopdf youtube-dl
+defaults write com.apple.finder QuitMenuItem -bool yes
