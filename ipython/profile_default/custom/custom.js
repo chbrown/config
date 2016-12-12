530c676074485b8e35b88d86396415e0a1b5290c
Christopher Brown <io@henrian.com>
2016-12-11 20:44:40 -0600

    Remove IPython config

--- ipython/profile_default/custom/custom.js
+++
@@ -1,9 +0,0 @@
-require(['base/js/namespace', 'base/js/events'], function(IPython, events) {
-  events.on('app_initialized.NotebookApp', function() {
-    IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
-  });
-});
-
-require(['jquery'], function($) {
-  $('#header-container').hide();
-});
--- ipython/profile_default/ipython_config.py
+++
@@ -1,9 +0,0 @@
-# disable pager
-from __future__ import print_function
-from IPython.core import page
-page.page = print
-
-c = get_config()
-# we can do this easily with 'alias ipy='ipython --no-confirm-exit'
-# but what if we drop into the IPython REPL from a Python script?
-c.InteractiveShell.confirm_exit = False
