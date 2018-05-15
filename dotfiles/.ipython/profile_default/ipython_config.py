b38af23ae33d70fcd157034b4cb210516d6c3bcb
Christopher Brown <io@henrian.com>
2018-05-15 18:58:11 -0500

    Add 'InteractiveShell.ast_node_interactivity' IPython config
    * That setting is also loaded by Jupyter, for the same effect
    * Add comments for both that and the existing 'confirm_exit' setting

--- dotfiles/.ipython/profile_default/ipython_config.py
+++ dotfiles/.ipython/profile_default/ipython_config.py
@@ -1,1 +1,4 @@
+# Don't prompt for confirmation when exiting with Ctrl-D
 c.TerminalInteractiveShell.confirm_exit = False
+# If the last statement is an assignment with a single symbol on the LHS, print it:
+c.InteractiveShell.ast_node_interactivity = 'last_expr_or_assign'
