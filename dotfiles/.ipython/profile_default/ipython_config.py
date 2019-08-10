b9bbfb042b3c2369058342c0e0a56f4b86b6b2a2
Christopher Brown <io@henrian.com>
2019-08-10 15:57:12 -0400

    Set PlainTextFormatter.max_seq_length to 100 in IPython config

--- dotfiles/.ipython/profile_default/ipython_config.py
+++ dotfiles/.ipython/profile_default/ipython_config.py
@@ -2,3 +2,5 @@
 c.TerminalInteractiveShell.confirm_exit = False
 # If the last statement is an assignment with a single symbol on the LHS, print it:
 c.InteractiveShell.ast_node_interactivity = 'last_expr_or_assign'
+# Decrease maximum shown items of sequence from default of 1000
+c.PlainTextFormatter.max_seq_length = 100  # requires Jupyter Lab restart to take effect
